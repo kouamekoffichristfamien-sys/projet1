cae0577f2bd8b358bcb6dd0c7c8894f2cd166fdc
famien <kouamekoffichristfamien@gmail.com>
2026-01-09 08:27:29 +0000

    Ajout du return à form.js

--- form.js
+++ form.js
@@ -3,7 +3,12 @@ function send() {
     let prenom = document.getElementById('prenom').value;
     let dateNaissance = new Date(document.getElementById('date_naissance').value);
     let dateActuelle = new Date();
-
+    
+    if(name=="" || prenom=="" || dateNaissance=="")
+    {
+        return alert("Veuillez remplir tous les champs");
+    };
+   
     let age = dateActuelle.getFullYear() - dateNaissance.getFullYear();
 
     alert("Bonjour " + name + " " + prenom +  ", vous aurez " + age + " ans cette annee");

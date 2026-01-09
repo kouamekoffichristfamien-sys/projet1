c3b0e209a3d565c8eac1d38774aa27c4fa25fee1
dorgeles <Yannodjegbi@gmail.com>
2026-01-09 00:45:55 +0000

    insertion du js pour le porjet1

---
+++ form.js
@@ -0,0 +1,10 @@
+function send() {
+    let name = document.getElementById('name').value;
+    let prenom = document.getElementById('prenom').value;
+    let dateNaissance = new Date(document.getElementById('date_naissance').value);
+    let dateActuelle = new Date();
+
+    let age = dateActuelle.getFullYear() - dateNaissance.getFullYear();
+
+    alert("Bonjour " + name + " " + prenom +  ", vous aurez " + age + " cette annee");
+}
